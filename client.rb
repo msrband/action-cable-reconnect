5e973a6cb66ae27ddd600be06da5fbc1384413ec
Philipp Weissensteiner <mail@philippweissensteiner.com>
2018-02-23 19:24:02 +0100

    Update client

--- client.rb
+++ client.rb
@@ -1,35 +1,31 @@
-require 'byebug'
 require 'action_cable_client'
 
 puts "Process: #{Process.pid}"
 
-100_000.times do |i|
-  EventMachine.run do
-    uri = "ws://localhost:3000/cable"
-    client = ActionCableClient.new(uri, 'ChatChannel')
+EventMachine.run do
+  uri = "ws://localhost:3000/cable"
+  client = ActionCableClient.new(uri, 'ChatChannel')
 
-    # the connected callback is required, as it triggers
-    # the actual subscribing to the channel
-    client.connected do
-      puts "#{i} Client connected"
-    end
+  # the connected callback is required, as it triggers
+  # the actual subscribing to the channel
+  client.connected do
+    puts "Client connected"
+  end
 
-    client.subscribed do
-      puts "#{i} Client subscribed"
-    end
+  client.subscribed do
+    puts "Client subscribed"
+  end
 
-    client.received do |msg|
-      puts "#{i} Client received: #{msg}"
-      EM::stop_event_loop
-    end
+  client.received do |msg|
+    puts "Client received: #{msg}"
+  end
 
-    client.errored do |msg|
-      puts "Client errored: #{msg}"
-    end
+  client.errored do |msg|
+    puts "Client errored: #{msg}"
+  end
 
-    client.disconnected do
-      puts "#{i} Client disconnected"
-      puts
-    end
+  client.disconnected do
+    puts "Client disconnected"
+    puts
   end
 end
